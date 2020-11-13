948a0ae4e5a7f957f405e6ce20e676913712fb71
NieXianFeng <niexianfeng@rocpow.com>
2020-11-13 09:49:40 +0800

    second

--- gitTestPro/main.cpp
+++ gitTestPro/main.cpp
@@ -1,11 +1,11 @@
-#include "widget.h"
+﻿#include "widget.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Widget w;
-    w.show();
+    Widget w1;
+    w1.show();
     return a.exec();
 }
